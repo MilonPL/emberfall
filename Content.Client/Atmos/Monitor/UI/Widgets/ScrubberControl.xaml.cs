8c4838fd09a8c298227177ad7cdf63e867d9efef
Ignaz Kraft <ignaz.k@live.de>
2024-11-18 03:13:11 +0100

    atmos circulation

--- Content.Client/Atmos/Monitor/UI/Widgets/ScrubberControl.xaml.cs
+++ Content.Client/Atmos/Monitor/UI/Widgets/ScrubberControl.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Content.Client.Stylesheets;
 using Content.Shared.Atmos;
 using Content.Shared.Atmos.Monitor;
 using Content.Shared.Atmos.Monitor.Components;
@@ -20,14 +21,15 @@ public sealed partial class ScrubberControl : BoxContainer
     private string _address;
 
     public event Action<string, IAtmosDeviceData>? ScrubberDataChanged;
-	public event Action<IAtmosDeviceData>? ScrubberDataCopied;
+    public event Action<IAtmosDeviceData>? ScrubberDataCopied;
 
     private CheckBox _enabled => CEnableDevice;
     private CollapsibleHeading _addressLabel => CAddress;
     private OptionButton _pumpDirection => CPumpDirection;
     private FloatSpinBox _volumeRate => CVolumeRate;
+    private FloatSpinBox _targetPressure => CTargetPressure;
     private CheckBox _wideNet => CWideNet;
-	private Button _copySettings => CCopySettings;
+    private Button _copySettings => CCopySettings;
 
     private GridContainer _gases => CGasContainer;
     private Dictionary<Gas, Button> _gasControls = new();
@@ -65,23 +67,30 @@ public sealed partial class ScrubberControl : BoxContainer
         };
         _volumeRate.IsValid += value => value >= 0;
 
+        _targetPressure.Value = _data.TargetPressure;
+        _targetPressure.OnValueChanged += _ =>
+        {
+            _data.TargetPressure = _targetPressure.Value;
+            ScrubberDataChanged?.Invoke(address, _data);
+        };
+
         foreach (var value in Enum.GetValues<ScrubberPumpDirection>())
         {
-            _pumpDirection.AddItem(Loc.GetString($"{value}"), (int) value);
+            _pumpDirection.AddItem(Loc.GetString($"{value}"), (int)value);
         }
 
-        _pumpDirection.SelectId((int) _data.PumpDirection);
+        _pumpDirection.SelectId((int)_data.PumpDirection);
         _pumpDirection.OnItemSelected += args =>
         {
             _pumpDirection.SelectId(args.Id);
-            _data.PumpDirection = (ScrubberPumpDirection) args.Id;
+            _data.PumpDirection = (ScrubberPumpDirection)args.Id;
             ScrubberDataChanged?.Invoke(_address, _data);
         };
-		
-		_copySettings.OnPressed += _ =>
-		{
-			ScrubberDataCopied?.Invoke(_data);
-		};
+
+        _copySettings.OnPressed += _ =>
+        {
+            ScrubberDataCopied?.Invoke(_data);
+        };
 
         foreach (var value in Enum.GetValues<Gas>())
         {
@@ -90,15 +99,43 @@ public sealed partial class ScrubberControl : BoxContainer
                 Name = value.ToString(),
                 Text = Loc.GetString($"{value}"),
                 ToggleMode = true,
-                HorizontalExpand = true,
-                Pressed = _data.FilterGases.Contains(value)
+                HorizontalExpand = true
             };
-            gasButton.OnToggled += args =>
+            if (_data.PriorityGases.Contains(value))
             {
-                if (args.Pressed)
-                    _data.FilterGases.Add(value);
-                else
-                    _data.FilterGases.Remove(value);
+                gasButton.StyleClasses.Add("ButtonColorGreen");
+            }
+            if (_data.DisabledGases.Contains(value))
+            {
+                gasButton.StyleClasses.Add(StyleBase.ButtonCaution);
+            }
+            gasButton.OnPressed += args =>
+            {
+                int state = 0;
+                if (_data.DisabledGases.Contains(value))
+                {
+                    state = 2;
+                }
+                else if (_data.PriorityGases.Contains(value))
+                {
+                    state = 1;
+                }
+
+                _data.PriorityGases.Remove(value);
+                _data.DisabledGases.Remove(value);
+                gasButton.StyleClasses.Remove("ButtonColorGreen");
+                gasButton.StyleClasses.Remove(StyleBase.ButtonCaution);
+                gasButton.Pressed = false;
+                if (state == 0)
+                {
+                    _data.PriorityGases.Add(value);
+                    gasButton.StyleClasses.Add("ButtonColorGreen");
+                }
+                else if (state == 1)
+                {
+                    _data.DisabledGases.Add(value);
+                    gasButton.StyleClasses.Add(StyleBase.ButtonCaution);
+                }
 
                 ScrubberDataChanged?.Invoke(_address, _data);
             };
@@ -114,18 +151,31 @@ public sealed partial class ScrubberControl : BoxContainer
         _enabled.Pressed = _data.Enabled;
 
         _data.PumpDirection = data.PumpDirection;
-        _pumpDirection.Select((int) _data.PumpDirection);
+        _pumpDirection.Select((int)_data.PumpDirection);
 
         _data.VolumeRate = data.VolumeRate;
         _volumeRate.Value = _data.VolumeRate;
 
+        _data.TargetPressure = data.TargetPressure;
+        _targetPressure.Value = _data.TargetPressure;
+
         _data.WideNet = data.WideNet;
         _wideNet.Pressed = _data.WideNet;
-        _data.FilterGases = data.FilterGases;
+        _data.PriorityGases = data.PriorityGases;
+        _data.DisabledGases = data.DisabledGases;
 
         foreach (var value in Enum.GetValues<Gas>())
         {
-            _gasControls[value].Pressed = data.FilterGases.Contains(value);
+            _gasControls[value].StyleClasses.Remove("ButtonColorGreen");
+            _gasControls[value].StyleClasses.Remove(StyleBase.ButtonCaution);
+            if (data.DisabledGases.Contains(value))
+            {
+                _gasControls[value].StyleClasses.Add(StyleBase.ButtonCaution);
+            }
+            else if (data.PriorityGases.Contains(value))
+            {
+                _gasControls[value].StyleClasses.Add("ButtonColorGreen");
+            }
         }
     }
 }
